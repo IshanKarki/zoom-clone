a6e1acd2202343403fab8e122ade683764d3bf18
Rivaan Ranawat <namanrivaan@gmail.com>
2020-11-01 15:10:42 +0530

    designing create meeting screen and generating meeting code

---
+++ lib/screens/join_meeting_screen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class JoinMeetingScreen extends StatefulWidget {
+  @override
+  _JoinMeetingScreenState createState() => _JoinMeetingScreenState();
+}
+
+class _JoinMeetingScreenState extends State<JoinMeetingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
--- lib/screens/meeting_screen.dart
+++ lib/screens/meeting_screen.dart
@@ -1,15 +1,65 @@
 import "package:flutter/material.dart";
+import 'package:zoom_clone/screens/create_meeting_screen.dart';
+import 'package:zoom_clone/screens/join_meeting_screen.dart';
+import 'package:zoom_clone/universal_variables.dart';
 
 class MeetingScreen extends StatefulWidget {
   @override
   _MeetingScreenState createState() => _MeetingScreenState();
 }
 
-class _MeetingScreenState extends State<MeetingScreen> {
+class _MeetingScreenState extends State<MeetingScreen>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+  tabBuilder(String name) {
+    return Container(
+      width: 150,
+      height: 50,
+      child: Card(
+        child: Center(
+          child: Text(
+            name,
+            style: ralewayStyle(
+              15,
+              Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text(
+          "Zoom Clone",
+          style: ralewayStyle(20, Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            tabBuilder("Join Meeting"),
+            tabBuilder("Create Meeting"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          JoinMeetingScreen(),
+          CreateMeeetingScreen(),
+        ],
+      ),
     );
   }
 }
